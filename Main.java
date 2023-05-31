a1e82b7e6b0b0231ac0257dd7fa3c3e9c5cc27dd
Wneq1 <127328405+Wneq1@users.noreply.github.com>
2023-05-31 18:44:15 +0000

    6.2

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class WrongStudentName extends Exception { }
-class WrongStudentAge extends Exception { }
+class WrongStudentAge extends Exception  { }
+class WrongMiniMenu extends Exception    { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -23,20 +25,33 @@ class Main {
                 System.out.println("Błędne imie studenta!");
             }  catch(WrongStudentAge e) {
                 System.out.println("Błędny wiek studenta!");}
+          catch(WrongMiniMenu e) {
+                System.out.println("Błędny wybor znaku");}
         }
     }
 
-    public static int menu() {
-        System.out.println("Wciśnij:");
-        System.out.println("1 - aby dodać studenta");
-        System.out.println("2 - aby wypisać wszystkich studentów");
-        System.out.println("3 - aby wyszukać studenta po imieniu");
-        System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
-    }
+   public static int menu() throws WrongMiniMenu {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Wciśnij:");
+    System.out.println("1 - aby dodać studenta");
+    System.out.println("2 - aby wypisać wszystkich studentów");
+    System.out.println("3 - aby wyszukać studenta po imieniu");
+    System.out.println("0 - aby wyjść z programu");
 
+    try {
+        int z = scan.nextInt();
+
+        if (z > 3 || z < 0) {
+            throw new WrongMiniMenu();
+        }
+
+        return z;
+    } catch (InputMismatchException e) {
+        throw new WrongMiniMenu();
+    }
+}
     public static String ReadName() throws WrongStudentName {
-        scan.nextLine();
+      
         System.out.println("Podaj imie: ");
         String name = scan.nextLine();
         if(name.contains(" "))
@@ -52,11 +67,11 @@ class Main {
           throw new WrongStudentAge();
     return age;
   }
-    public static void exercise1() throws IOException, WrongStudentName,WrongStudentAge {
+    public static void exercise1() throws IOException, 
+    WrongStudentName,WrongStudentAge {
         var name = ReadName();
         var age = ReadAge();
-       //  if(age<0||age>100) 
-        //  throw new WrongStudentAge();
+    
       
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
